8f913d48ba603f21c4d97e860ed27a27c0783a4d
kamran <k.qurbanli@gmail.com>
2024-07-17 11:43:15 +0300

    Add Circular Initials icon

--- app/src/main/java/com/kamran/energon/ui/MainView.kt
+++ app/src/main/java/com/kamran/energon/ui/MainView.kt
@@ -2,27 +2,25 @@ package com.kamran.energon.ui
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.BottomSheetScaffold
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.ExtendedFloatingActionButton
-import androidx.compose.material3.Icon
-import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.rememberModalBottomSheetState
+import androidx.compose.material3.rememberBottomSheetScaffoldState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -31,32 +29,27 @@ import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.MapUiSettings
+import com.kamran.energon.widgets.CircularInitialsIcon
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun MainView(viewModel: MainViewModel = hiltViewModel()) {
-	var showBottomSheet by remember { mutableStateOf(false) }
-	val sheetState = rememberModalBottomSheetState()
+	val sheetState = rememberBottomSheetScaffoldState()
 	val scope = rememberCoroutineScope()
-	Scaffold(modifier = Modifier.fillMaxSize(),
-		floatingActionButton = {
-			ExtendedFloatingActionButton(
-				text = { Text("Show bottom sheet") },
-				icon = { Icon(Icons.Filled.Add, contentDescription = "") },
-				onClick = {
-					showBottomSheet = true
-				}
-			)
-		}) { innerPadding ->
+	Scaffold(
+		modifier = Modifier.fillMaxSize(),
+	) { innerPadding ->
 
-		if (showBottomSheet) {
-			ModalBottomSheet(
-				onDismissRequest = {
-					showBottomSheet = false
-				},
-				sheetState = sheetState
-			) {
+		BottomSheetScaffold(
+			//modifier = Modifier.padding(innerPadding),
+			sheetSwipeEnabled = true,
+			scaffoldState = sheetState,
+			sheetPeekHeight = 120.dp,
+			sheetDragHandle = null,
+			sheetContent = {
+				Column {
 					Box(
 						modifier = Modifier
 							.fillMaxWidth()
@@ -64,43 +57,57 @@ fun MainView(viewModel: MainViewModel = hiltViewModel()) {
 							.padding(horizontal = 12.dp)
 							.background(color = Color.White, shape = RoundedCornerShape(80.dp))
 							.clip(CircleShape)
+							.clickable(
+								onClick = {
+									scope.launch {
+										sheetState.bottomSheetState.expand()
+									}
+								}
+							)
 					) {
-						Text(
-							text = "Filled",
-							modifier = Modifier
-								.padding(16.dp),
-							textAlign = TextAlign.Center,
-						)
+						Row(
+							Modifier.fillMaxWidth(),
+							horizontalArrangement = Arrangement.SpaceBetween,
+							verticalAlignment = Alignment.CenterVertically,
+						) {
+							Text(
+								text = "Search",
+								modifier = Modifier
+									.padding(16.dp),
+								textAlign = TextAlign.Center,
+							)
+							CircularInitialsIcon(
+								modifier = Modifier.padding(end = 4.dp),
+								initials = "KG",
+								size = 48.dp)
+						}
 					}
+					Box(modifier = Modifier.height(600.dp))
+				}
 
 			}
-		}
-
-		Box(modifier = Modifier.fillMaxSize()) {
-			GoogleMap(
-				modifier = Modifier.fillMaxSize(),
-				// .padding(innerPadding),
-				uiSettings = MapUiSettings(
-					compassEnabled = true,
-					rotationGesturesEnabled = true,
-					mapToolbarEnabled = true,
-					myLocationButtonEnabled = true
+		) {
+			Box(modifier = Modifier.fillMaxSize()) {
+				GoogleMap(
+					modifier = Modifier.fillMaxSize(),
+					// .padding(innerPadding),
+					uiSettings = MapUiSettings(
+						compassEnabled = true,
+						rotationGesturesEnabled = true,
+						mapToolbarEnabled = true,
+						myLocationButtonEnabled = true
+					)
 				)
-			)
-			Box(
-				modifier = Modifier
-					.fillMaxWidth()
-					.padding(innerPadding)
-					.padding(horizontal = 12.dp)
-					.background(color = Color.White, shape = RoundedCornerShape(80.dp))
-					.clip(CircleShape)
-			) {
-				Text(
-					text = "Filled",
+				Box(
 					modifier = Modifier
-						.padding(16.dp),
-					textAlign = TextAlign.Center,
-				)
+						.fillMaxWidth()
+						.padding(innerPadding)
+						.padding(horizontal = 12.dp)
+						.background(color = Color.White, shape = RoundedCornerShape(80.dp))
+						.clip(CircleShape)
+				) {
+
+				}
 			}
 		}
 	}
---
+++ app/src/main/java/com/kamran/energon/widgets/CircularInitialsIcon.kt
@@ -0,0 +1,38 @@
+package com.kamran.energon.widgets
+
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.times
+
+@Composable
+fun CircularInitialsIcon(
+	initials: String,
+	size: Dp,
+	modifier: Modifier = Modifier,
+	backgroundColor: Color = Color.Gray,
+	textColor: Color = Color.White
+) {
+	Box(
+		modifier = modifier.size(size),
+		contentAlignment = Alignment.Center
+	) {
+		Canvas(modifier = Modifier.size(size)) {
+			drawCircle(color = backgroundColor)
+		}
+		Text(
+			text = initials.take(2).uppercase(),
+			color = textColor,
+			fontSize = size.value * 0.4f.sp,
+			fontWeight = FontWeight.Bold
+		)
+	}
+}
